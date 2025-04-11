e3a42ce268eeaec286787fdde0d6200274b91cd3
DivyGandhi <divygandhi4978@gmail.com>
2025-04-11 10:27:05 +0530

    done

--- frontend/backend/models/UserLogs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userlog = mongoose.Schema({
-  userId: { type: String, require },
-  date: { type: Date, default: Date.now() },
-});
-
-module.exports = mongoose.model("UserLog", userlog);
